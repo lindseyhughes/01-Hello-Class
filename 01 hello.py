1e2874c245f44a0818b37513e45031f6bb135712
Lindsey <linehugh@iu.edu>
2018-01-11 12:45:11 -0500

    First Assignment

--- 01 hello.py
+++ 01 hello.py
@@ -4,4 +4,9 @@ import sys
 
 assert sys.version_info >= (3,4), "This script requires at least Python 3.4"
 
-print('Hello, Class!')
+print('Hi, my name is Lindsey Hughes')
+print('I have always loved the Fallout series, but my favorite game is either Dying Light or Heavy Rain')
+print('I am worried about the fact that this class is at eight AM')
+print('I am excited about all the free time I have this semester')
+print('My Stackoverflow number is 9205361')
+print('https://github.com/lindseyhughes')
